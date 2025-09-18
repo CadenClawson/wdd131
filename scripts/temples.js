6685b0b4d751350b43c74ccd07f6a7bc0daecbf9
CadenClawson <darthvaderisboss2@gmail.com>
2025-09-17 18:59:23 -0600

    Add files via upload
    all the stuff for the week 2 assignment, Temple Page

---
+++ scripts/temples.js
@@ -0,0 +1,11 @@
+<header>
+  <h1>Temple Album</h1>
+  <button id="menuButton" aria-label="Menu">☰</button>
+  <nav id="primaryNav">
+    <a href="#">Home</a>
+    <a href="#">Old</a>
+    <a href="#">New</a>
+    <a href="#">Large</a>
+    <a href="#">Small</a>
+  </nav>
+</header>
